214daf104ebeb5316217cd1118fc205a388e4882
jaflavier-and <98226913+jaflavier-and@users.noreply.github.com>
2024-06-18 05:16:59 +0000

    Completed Level 1 debugging

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,22 +12,40 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
+
+# Define limits
+MAX_ITEM_VALUE = 100000 # Maximum value
+MAX_QUANTITY = 100 # maximum quantity
+MIN_QUANTITY = 0 # minimum quantity
+MAX_TOTAL_VALUE = 1e6 # Maximum net value of an order
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
-    net = 0
+    # net = 0
+    customerPayment = Decimal('0') #total value customer paid
+    orderTotalValue = Decimal('0')# total value of order
+
 
     for item in order.items:
+        # calculate payment from customer
         if item.type == 'payment':
-            net += item.amount
-        elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if -MAX_ITEM_VALUE <= item.amount <= MAX_ITEM_VALUE :
+                customerPayment += Decimal(str(item.amount))
+
+        # calculate item values
+        elif item.type == 'product' and MIN_QUANTITY <= item.quantity <= MAX_QUANTITY and MIN_QUANTITY < item.amount <= MAX_ITEM_VALUE:
+            orderTotalValue += Decimal(str(item.amount)) * Decimal(str(item.quantity))
         else:
             return "Invalid item type: %s" % item.type
 
-    if net != 0:
-        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    # check for total value exceeding maximum total cost
+    if orderTotalValue > MAX_TOTAL_VALUE or abs(customerPayment) > MAX_TOTAL_VALUE:
+        return "Total amount payable for an order exceeded"
+
+    if customerPayment != orderTotalValue:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, customerPayment - orderTotalValue)
     else:
         return "Order ID: %s - Full payment received!" % order.id
